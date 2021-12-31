d5a89ec95b2baab0b0805929e0c0bc6e301e02cd
hijuliancode <hijuliansosa@gmail.com>
2021-12-31 14:35:35 -0500

    RN | Counter: Styles, Stylesheet

--- Counter/src/screens/CounterScreen.tsx
+++ Counter/src/screens/CounterScreen.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {View, Text, Button, TouchableOpacity} from 'react-native';
+import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
 
 export const CounterScreen = () => {
   const [counter, setCounter] = useState(0);
@@ -9,36 +9,35 @@ export const CounterScreen = () => {
   };
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-      <Text
-        style={{
-          textAlign: 'center',
-          fontSize: 32,
-          top: -12,
-        }}>
-        Contador: {counter}
-      </Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Contador: {counter}</Text>
       <TouchableOpacity onPress={handleIncrease}>
-        <View
-          style={{
-            backgroundColor: '#60a5fa',
-            padding: 12,
-            borderRadius: 10,
-            margin: 10,
-          }}>
-          <Text
-            style={{
-              color: '#fff',
-            }}>
-            Aumentar
-          </Text>
+        <View style={styles.button}>
+          <Text style={styles.buttonText}>Aumentar</Text>
         </View>
       </TouchableOpacity>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: 32,
+    top: -12,
+  },
+  button: {
+    backgroundColor: '#60a5fa',
+    padding: 12,
+    borderRadius: 10,
+    margin: 10,
+  },
+  buttonText: {
+    color: '#fff',
+  },
+});
