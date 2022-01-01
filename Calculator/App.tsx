2dcc0da86b65773353069fd00795cb68fadfdd6d
hijuliancode <hijuliansosa@gmail.com>
2022-01-01 11:07:39 -0500

    RN | Calculator: Starting: MainScreen, Starting Theme

--- Calculator/App.tsx
+++ Calculator/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {SafeAreaView, StatusBar} from 'react-native';
+import {MainScreen} from './src/screens';
+import {styles} from './src/theme';
 
 const App = () => {
   return (
-    <View>
-      <Text>Hello Calculator!!!</Text>
-    </View>
+    <SafeAreaView style={styles.mainBackground}>
+      <StatusBar barStyle="light-content" backgroundColor="#060116" />
+      <MainScreen />
+    </SafeAreaView>
   );
 };
 
---
+++ Calculator/src/theme/AppTheme.tsx
@@ -0,0 +1,14 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  mainBackground: {
+    backgroundColor: '#060116',
+    flex: 1,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
