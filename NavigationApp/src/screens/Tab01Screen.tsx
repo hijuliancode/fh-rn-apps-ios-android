c0a387d3a4e118b869af9a3aca40358a0436b08f
hijuliancode <hijuliansosa@gmail.com>
2022-01-03 00:13:51 -0500

    RN | ReactNavigation: Add Icons : IOS

--- NavigationApp/src/screens/Tab01Screen.tsx
+++ NavigationApp/src/screens/Tab01Screen.tsx
@@ -12,6 +12,15 @@ export default function Tab01Screen() {
       <Text style={stylesTheme.title}>Tab01Screen</Text>
       <Text>
         <Icon name="airplane-outline" size={32} color="#900" />
+        <Icon name="american-football-outline" size={32} color="#900" />
+        <Icon name="baseball-outline" size={32} color="#900" />
+        <Icon name="beer-outline" size={32} color="#900" />
+        <Icon name="bonfire-outline" size={32} color="#900" />
+        <Icon name="color-palette-outline" size={32} color="#900" />
+        <Icon name="snow-outline" size={32} color="#900" />
+        <Icon name="fast-food-outline" size={32} color="#900" />
+        <Icon name="finger-print-outline" size={32} color="#900" />
+        <Icon name="shirt-outline" size={32} color="#900" />
       </Text>
     </View>
   );
