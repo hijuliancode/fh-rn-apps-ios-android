a0dfd6586920416ef4cd12575665a0d98ba6a1a5
hijuliancode <hijuliansosa@gmail.com>
2022-01-02 23:04:47 -0500

    RN | ReactNavigation: TopTabNavigation: Customizing Styles

--- NavigationApp/src/navigator/TopTabNavigator.tsx
+++ NavigationApp/src/navigator/TopTabNavigator.tsx
@@ -1,12 +1,54 @@
 import React from 'react';
 import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
 import {ChatScreen, ContactsScreen, AlbumsScreen} from '../screens';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {Text} from 'react-native';
 
 const Tab = createMaterialTopTabNavigator();
 
 export const TopTabNavigator = () => {
+  const {top: paddingTop} = useSafeAreaInsets();
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      style={
+        {
+          // paddingTop,
+        }
+      }
+      sceneContainerStyle={{backgroundColor: '#fff'}}
+      screenOptions={({route}) => ({
+        tabBarStyle: {
+          backgroundColor: '#ffa',
+          elevation: 0,
+          shadowColor: 'transparent',
+        },
+        tabBarPressColor: '#0f0',
+        tabBarShowIcon: true,
+        tabBarIndicatorStyle: {
+          backgroundColor: '#0f0',
+        },
+        tabBarIcon: ({focused, color, size}) => {
+          let iconName: string = 'T';
+
+          switch (route.name) {
+            case 'ChatScreen':
+              iconName = 'CHT';
+              break;
+
+            case 'ContactsScreen':
+              iconName = 'CNTS';
+              break;
+
+            case 'AlbumsScreen':
+              iconName = 'ALB';
+              break;
+            default:
+              break;
+          }
+
+          return <Text style={{color}}>{iconName}</Text>;
+        },
+      })}>
       <Tab.Screen
         name="ChatScreen"
         options={{
