8f452896d2ad35629959b6ee1918fe565ed64b3f
hijuliancode <hijuliansosa@gmail.com>
2022-01-02 23:24:02 -0500

    RN | ReactNavigation: Add Icons : Android

--- NavigationApp/src/screens/Tab01Screen.tsx
+++ NavigationApp/src/screens/Tab01Screen.tsx
@@ -1,13 +1,18 @@
 import React, {useEffect} from 'react';
+import Icon from 'react-native-vector-icons/Ionicons';
 import {View, Text} from 'react-native';
+import {stylesTheme} from '../theme';
 
 export default function Tab01Screen() {
   useEffect(() => {
     console.log('Tab01Screen Effect');
   }, []);
   return (
-    <View>
-      <Text>Tab01Screen</Text>
+    <View style={stylesTheme.globalWrapper}>
+      <Text style={stylesTheme.title}>Tab01Screen</Text>
+      <Text>
+        <Icon name="airplane-outline" size={32} color="#900" />
+      </Text>
     </View>
   );
 }
