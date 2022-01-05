c90c9c541aa1f15a6818929c641873c9a5cd2063
hijuliancode <hijuliansosa@gmail.com>
2022-01-05 08:55:28 -0500

    RN | ReactNavigation: Imports Order

--- NavigationApp/App.tsx
+++ NavigationApp/App.tsx
@@ -1,18 +1,18 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
+// import {StackNavigator} from './src/navigator';
+// import {DrawerNavigator} from './src/navigator';
 // import {Tabs} from './src/navigator';
 import {SideNav} from './src/navigator';
-// import {DrawerNavigator} from './src/navigator';
-// import {StackNavigator} from './src/navigator';
 
 export const App = () => {
   return (
     <NavigationContainer>
       {/* <StackNavigator /> */}
       {/* <DrawerNavigator /> */}
-      <SideNav />
       {/* <Tabs /> */}
+      <SideNav />
     </NavigationContainer>
   );
 };
