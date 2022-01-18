63eec3d928d85bd1f9c5af9b4322a94b5c15da08
hijuliancode <hijuliansosa@gmail.com>
2022-01-18 17:24:14 -0500

    RB | Context : useContext

--- NavigationApp/src/screens/SettingsScreen.tsx
+++ NavigationApp/src/screens/SettingsScreen.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {View, Text} from 'react-native';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {AuthContext} from '../context/AuthContext';
 
 export const SettingsScreen = () => {
   const insets = useSafeAreaInsets();
+
+  const context = useContext(AuthContext);
+
   return (
     <View style={{marginTop: insets.top}}>
       <Text>Settings Screen</Text>
+      <Text>{JSON.stringify(context.authState, null, 4)}</Text>
     </View>
   );
 };
